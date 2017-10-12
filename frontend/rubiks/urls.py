b5986db24aadf0fbff4f4ed94ac1a3d48311af1e
vnikme <accs@vnik.me>
2017-10-12 07:39:48 -0400

    Base pages for rubiks.

--- frontend/rubiks/urls.py
+++ frontend/rubiks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import rubiks.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^.*', rubiks.views.main_page),
 ]
---
+++ frontend/rubiks/views.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.utils.six.moves.urllib.parse import parse_qsl, urlparse, urlunparse
+from django.utils.cache import patch_cache_control
+from django.views.decorators.csrf import csrf_exempt
+
+def do_general(request, body):
+    t = get_template('general.html')
+    html = t.render({'body': body})
+    response = HttpResponse(html, content_type = 'text/html')
+    patch_cache_control(response, max_age=0)
+    return response
+
+def privacy_page(request):
+    return do_general(request, 'privacy.html')
+
+def main_page(request):
+    return do_general(request, 'main.html')
+
