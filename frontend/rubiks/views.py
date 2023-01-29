f7e4ebb55fc3f646ff1202b6597848ea94c57312
vnikme <accs@vnik.me>
2023-01-29 18:06:35 -0500

    Fix exceptions.

--- frontend/rubiks/views.py
+++ frontend/rubiks/views.py
@@ -6,13 +6,13 @@ from django.template.loader import get_template
 from django.utils.six.moves.urllib.parse import parse_qsl, urlparse, urlunparse
 from django.utils.cache import patch_cache_control
 from django.views.decorators.csrf import csrf_exempt
-import httplib, time, base64, json
+import urllib.request, time, base64, json
 
 
 def do_general(request, body):
     t = get_template('general.html')
     html = t.render({'body': body})
-    response = HttpResponse(html, content_type = 'text/html')
+    response = HttpResponse(html, content_type='text/html')
     patch_cache_control(response, max_age=0)
     return response
 
@@ -33,13 +33,12 @@ def Solve(cube):
     try:
         if not cube:
             return None
-        conn = httplib.HTTPConnection("localhost", 17071)
-        conn.request("GET", "/solve?cube=%s" % cube, None)
-        res = conn.getresponse()
-        if res.status != 200:
+        request = urllib.request.Request("http://localhost:17071/solve?cube={}".format(cube), method='GET')
+        response = urllib.request.urlopen(request)
+        if response.status != 200:
             return None
-        return res.read()
-    except:
+        return response.read()
+    except Exception as e:
         return None
 
 
@@ -48,24 +47,22 @@ def solve_page(request):
     answer = Solve(cube)
     if not answer:
         answer = """{"state": "fail", "message": "could not reach backend or bad parameters"}"""
-    return HttpResponse(answer, content_type = "text/json")
+    return HttpResponse(answer, content_type="text/json")
 
 
 def Log(data):
     try:
         if not data:
             return None
-        conn = httplib.HTTPConnection("localhost", 17071)
-        ts = time.time()
         data = json.loads(base64.b64decode(data))
-        data["server_timestamp"] = ts
+        data["server_timestamp"] = time.time()
         data = base64.b64encode(json.dumps(data))
-        conn.request("POST", "/log", data)
-        res = conn.getresponse()
-        if res.status != 200:
+        request = urllib.request.Request("http://localhost:17071/log".format(cube), data=data)
+        response = urllib.request.urlopen(request)
+        if response.status != 200:
             return None
-        return res.read()
-    except:
+        return ressponse.read()
+    except Exception as e:
         return None
 
 
@@ -75,5 +72,5 @@ def log_page(request):
     answer = Log(data)
     if not answer:
         answer = """{"state": "fail", "message": "could not reach backend or bad parameters"}"""
-    return HttpResponse(answer, content_type = "text/json")
+    return HttpResponse(answer, content_type="text/json")
 
